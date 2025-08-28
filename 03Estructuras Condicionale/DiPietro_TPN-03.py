3afa7919e089aab9ab4a0f6702a403e61cbbbbc5
dipietromauro9-create <dipietromauro9@gmail.com>
2025-08-28 10:55:04 -0300

    Ejercicio 2

--- 03Estructuras Condicionale/DiPietro_TPN-03.py
+++ 03Estructuras Condicionale/DiPietro_TPN-03.py
@@ -4,3 +4,9 @@ if edad>=18:
     print("Usted es mayor de edad")
 else:
     print("Usted es menor de edad")
+#Ejercicio 2:#
+nota=int(input("Ingrese su nota: "))
+if nota>=6:
+    print("Usted esta aprobado")
+else:
+    print("Usted esta desaprobado")
