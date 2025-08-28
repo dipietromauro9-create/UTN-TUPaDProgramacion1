a2b9ec16c7a92d5adfe6632b1115e88127921465
dipietromauro9-create <dipietromauro9@gmail.com>
2025-08-28 11:23:04 -0300

    Ejercicio 4

--- 03Estructuras Condicionale/DiPietro_TPN-03.py
+++ 03Estructuras Condicionale/DiPietro_TPN-03.py
@@ -11,9 +11,21 @@ if nota>=6:
     print("Usted esta aprobado")
 else:
     print("Usted esta desaprobado")
+
 #Ejercicio 3:#
 numpar=int(input("Ingrese un numero par "))
 if numpar%2==0:
     print("Usted ingreso un numero par")
 else:
     print("Por favor, ingrese un número par")
+
+#Ejercicio 4:#
+edad4=int(input("Ingrese su edad "))
+if edad4<12:
+    print("Pertenece a la categoría niño/a")
+elif edad4<18:
+    print("Pertenece a la categoría adolecente")
+elif edad4<30:
+    print("Pertenece a la categoría adulto/a joven")
+else:
+    print("Pertenece a la categoría adulto/a")
