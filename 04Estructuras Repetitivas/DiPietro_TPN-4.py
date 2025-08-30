cdc6f21e8516c1f9bc5045c078c362d85eb9cda0
dipietromauro9-create <dipietromauro9@gmail.com>
2025-08-30 11:47:46 -0300

    Ejercicio 2

--- 04Estructuras Repetitivas/DiPietro_TPN-4.py
+++ 04Estructuras Repetitivas/DiPietro_TPN-4.py
@@ -1,3 +1,7 @@
 #Ejercicio 1#
 for i in range(0,101):
     print(i)
+#Ejercicio 2#
+numero=input("Ingrese un numero ")
+digitos=len(numero)
+print(f"La cantidad de digitos que tiene {numero} es {digitos}")
