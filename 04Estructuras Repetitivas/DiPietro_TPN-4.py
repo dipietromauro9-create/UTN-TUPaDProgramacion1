645bd1d2ae8c153b20e2dce98abad7448024cb2f
dipietromauro9-create <dipietromauro9@gmail.com>
2025-08-30 12:58:32 -0300

    ejercicio 5

--- 04Estructuras Repetitivas/DiPietro_TPN-4.py
+++ 04Estructuras Repetitivas/DiPietro_TPN-4.py
@@ -19,3 +19,13 @@ while num!=0:
     num=int(input("Ingrese numero que quiere agregar a la secuencia: "))
     sumatoria=sumatoria+num
 print(f"La sumatoria total es: {sumatoria}")
+#Ejercicio 5#
+import random
+numero_aleatorio=random.randint(0,9)
+print(numero_aleatorio)
+cont=0
+adivina=-1
+while adivina!=numero_aleatorio:
+    adivina=int(input("Adivina el numero: "))
+    cont=cont+1
+print(f"!Felicidades acertaste el numero¡ Cantidad de intentos {cont}")
