632d99acebeb27d4af032fc97bf878c552aee655
dipietromauro9-create <dipietromauro9@gmail.com>
2025-09-05 10:50:24 -0300

    Ejercicio 9

---
+++ 05Listas/09_Ejercicio.py
@@ -0,0 +1,7 @@
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+compras[2].append("jugo")
+print(compras)
+compras[1][1]="tallarines"
+print(compras)
+compras[0].remove("pan")
+print(compras)
