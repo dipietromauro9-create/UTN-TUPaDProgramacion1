7fd3c125e0f5dfb3a7c06efea156b4c82d0b2441
dipietromauro9-create <dipietromauro9@gmail.com>
2025-10-17 12:13:44 -0300

    ejercicio 2

---
+++ 09Recursividad/ejercicio_02.py
@@ -0,0 +1,9 @@
+def fibo(x):
+    if x==0:
+        return 0
+    elif x ==1:
+        return 1
+    else:
+        return fibo(x-1)+fibo(x-2)
+num=int(input("Ingrese un numero: "))
+print(fibo(num))
