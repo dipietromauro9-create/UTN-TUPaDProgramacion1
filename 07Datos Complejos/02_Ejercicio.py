6870de8935d7f41e1fdbcdf9b9888968b343dbd6
dipietromauro9-create <dipietromauro9@gmail.com>
2026-01-20 16:04:13 -0300

    Update 02_Ejercicio.py

--- 07Datos Complejos/02_Ejercicio.py
+++ 07Datos Complejos/02_Ejercicio.py
@@ -1,5 +1,9 @@
-dicc={"Manuela":3,
-      "Jose":5
-}
-dicc["luis"]=10
-print(dicc)
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+precios_frutas["Naranja"]=1200
+precios_frutas["Manzana"]=1500
+precios_frutas["Pera"]=2300
+precios_frutas["Banana"] = 1330
+precios_frutas["Manzana" ]= 1700
+precios_frutas["Melón"] = 2800
+for fruta,precio in precios_frutas.items():
+    print(f"{fruta}:{precio}")
